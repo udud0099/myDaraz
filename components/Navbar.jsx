656e6a97bb32d66ff356e85cbf6c5fe9d29528dc
udud0099 <udud0099@gmail.com>
2025-10-15 17:35:17 +0545

    test1

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         //   className="flex items-center gap-2 hover:text-gray-900 transition"
         // >
         //   <Image src={assets.user_icon} alt="user icon" />
-        //   Sign Out
+        //   Sign Out   
         // </button>
         <UserButton >
           <UserButton.MenuItems>
