af6d0c4ab0925b6e969e3716f28d72bcc6c5e1ee
udud0099 <udud0099@gmail.com>
2025-10-15 18:02:29 +0545

    update 3

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider> 
       <html lang="en">
         <body className={`${outfit.className} antialiased text-gray-700`} >
           <Toaster />
